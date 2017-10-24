93d555008becb202871750007aaa2afeca7f3a32
Mark Burrell (ORF) <markrb060@gmail.com>
2017-10-24 11:20:34 -0700

    fixed the recursive function and added non-recursrive equiv

--- InClassPY/recursive.py
+++ InClassPY/recursive.py
@@ -1,11 +1,25 @@
 printList=[]
 def recMult(n):
+    print(n)
     global printList
-    if n==1:
-        printList.append(2)
+    if n==0:
         return 0
     else:
-        printList.append(2*n)
-        return 2*recMult(n-1)
-recMult(4)
-print(printList)
+        return (2*n)+(recMult(n-1))
+print(recMult(4))
+
+
+def mult(n):
+    numbList = []
+    total=0
+    while n >= 0:
+        numbList.append(2*n)
+        n=n-1
+    numbList.append(0)
+    for i in numbList:
+        total=total+i
+    return total
+print(mult(4))
+    
+    
+    
