d98f479977314a98d752d684a1539ef84c41d527
Mark Burrell (ORF) <markrb060@gmail.com>
2017-10-26 10:57:12 -0700

    additon of selfSorted function

--- InClassPY/minMaxWithGlobal.py
+++ InClassPY/minMaxWithGlobal.py
@@ -9,5 +9,18 @@ def minMax():
     return sorted(numbList)
 print(minMax())
 
-        
+def MinMax():
+    global numbList
+    printList=[]
+    mini=numbList[0]
+    l=len(numbList)
+    for m in range(0,l):
+        mini=numbList[0]
+        for i in numbList:
+            if i<mini:
+                mini=i
+        printList.append(mini)
+        numbList.remove(mini)
+    return(printList)
+print(MinMax())
         
