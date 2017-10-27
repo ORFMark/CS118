b556b738ef95a0f0eaa658cce39df2eaffac5c93
Mark Burrell (ORF) <markrb060@gmail.com>
2017-10-27 09:41:40 -0700

    working encryption and decrypt programs with example key, encrypted and decrypted files, base file already included

--- Homework/Assignment_6_MRB_Decrypt.py
+++ Homework/Assignment_6_MRB_Decrypt.py
@@ -2,34 +2,36 @@ scrambleLine=[]
 lineCrypt=[]
 lineClear=[]
 Order=[]
-f=open('H:\ERAU_Year_1\Fall_Semester\CS118\GitHub2\Key.txt','r')
+f=open('H:\ERAU_Year_1\Fall_Semester\CS118\GitHub2\Key.txt','r') ##opens and copies the key
 stringKey=f.readline()
+stringKeyList=list(stringKey)
+stringKeyList.pop()
+stringKey=''.join(stringKeyList)
 lineOrder=f.readline()
 f.close()
-f=open('H:\ERAU_Year_1\Fall_Semester\CS118\GitHub2\encrypted.txt','r')
+f=open('H:\ERAU_Year_1\Fall_Semester\CS118\GitHub2\encrypted.txt','r') #opens the scrambled file and reads it
 i=f.readline()
 while i != '':
     scrambleLine.append(i)
     i=f.readline()
 f.close()
 f=open("H:\ERAU_Year_1\Fall_Semester\CS118\GitHub2\Decrypt.txt",'w+')
-for i in range(0,len(lineOrder)):
+for i in range(0,len(lineOrder)): ##puts the lines back in the right order
     Order.append(int(lineOrder[i]))
 for i in Order:
-    lineCrypt.append(scrambleLine[i])
-for i in range (0,len(lineCrypt)):
+    lineCrypt.append(scrambleLine[i]) 
+for i in range (0,len(lineCrypt)): ##unscrambles the lines
     for j in lineCrypt[i]:
         if j.isalpha()==False:
             lineClear.append(j)
             continue
         for m in range(1,len(stringKey),2):
-            print(len(stringKey),",", m, ',', stringKey[m],",",stringKey[m+1])
             if stringKey[m]==j:
-                lineClear.append(stringKey[m+1])
+                lineClear.append(stringKey[m-1])
                ## print(stringKey[m+1], ' = ', j)
     line=''.join(lineClear)
     f.write(line)
     lineClear.clear()
     ##line=''.join(scrambleLine[i]
 f.close()
-
+print("done")
--- Homework/Assignment_6_MRB_Encrypt.py
+++ Homework/Assignment_6_MRB_Encrypt.py
@@ -5,10 +5,11 @@ lineList=[]
 I=f.readline()
 while I != '': ##creates the list of lines in the file
     lineList.append(I)
+    print(I)
     I=f.readline()
 f.close()
 f=open('H:\ERAU_Year_1\Fall_Semester\CS118\GitHub2\encrypted.txt','w+') #creates the file to write the encrypted infomation to
-alphaList=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'] #creaation of scramble
+alphaList=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'] #creaation of scramble
 deltaList=alphaList.copy()
 keyList=[]
 cryptlist=[]
@@ -17,6 +18,7 @@ random.shuffle(alphaList)
 random.shuffle(deltaList)
 shuffleLine=lineList.copy()
 random.shuffle(shuffleLine)
+print(shuffleLine)
 for i in lineList: ##shuffles the lines
     for z in range(0,len(lineList)):
         if i == shuffleLine[z]:
@@ -25,18 +27,23 @@ for i in lineList: ##shuffles the lines
 for i in range(0,len(alphaList)): #key creation
     keyList.append(alphaList[i])
     keyList.append(deltaList[i])
-    print(alphaList[i], ' = ', deltaList[i])
+    #print(alphaList[i], ' = ', deltaList[i])
 for i in range (0,len(lineList)): #scrambles the file
     text=shuffleLine[i]
     for j in range (0,len(text)):
         if text[j].isalpha()==False:
             if text[j]!='\n':
                 cryptlist.append(text[j])
+               # print("found non-alpha character: ",text[j])
+            #else:
+               # print("found newline: ",str(text[j]))
         else:
             for k in range (0,len(alphaList)):
                 if text[j]==alphaList[k]:
                    cryptlist.append(deltaList[k])        
+    #print(cryptlist)
     crypt=''.join(cryptlist)
+    #print(crypt)
     f.write(crypt)
     f.write('\n')
     cryptlist.clear()
